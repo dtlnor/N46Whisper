9d39bd4fdee2bd70d969541da0d998d4ce717931
dtlnor <dtlnor@hotmail.com>
2023-11-18 11:22:21 +0900

    format document

--- Local-audio2ass.py
+++ Local-audio2ass.py
@@ -1,26 +1,27 @@
 import os
+
 # import ffmpeg
 # import subprocess
 import torch
 from faster_whisper import WhisperModel, tokenizer
 from tqdm import tqdm
 import time
+
 # import pandas as pd
 # import requests
 # from urllib.parse import quote_plus
 from pathlib import Path
+
 # import sys
 import pysubs2
 
-#@title **从本地上传文件(可多选）/Upload Local File（Can select multiple)**
+# @title **从本地上传文件(可多选）/Upload Local File（Can select multiple)**
 # @markdown <font size="2">If use file in google drive, ignore this cell and move to the next.
 # @markdown <br/>若已选择谷歌盘中的文件，则跳过此步执行下一单元格。</font>
 # use_drive = False
-file_names = [
-    R"C:\Users\dtlnor\OKEGui\tools\x26x\day2Recording2_muxed.mp4"
-]
+file_names = [R"C:\Users\dtlnor\OKEGui\tools\x26x\day2Recording2_muxed.mp4"]
 
-#@title **通用参数/Required settings:**
+# @title **通用参数/Required settings:**
 
 
 # @markdown **【IMPORTANT】:**<font size="2">Select uploaded file type.
@@ -45,7 +46,7 @@ language = "zh"  # @param {type:"string"}
 # @markdown <br/><font size="2">导出时浏览器会弹出允许同时下载多个文件的请求，需要同意
 export_srt = "No"  # @param ["No","Yes"]
 
-#@title **其他选项/Advanced settings**
+# @title **其他选项/Advanced settings**
 
 # @markdown <font size="2">Option for split line text by spaces. The splited lines all use the same time stamp, with 'adjust_required' label as remark for manual adjustment.
 # @markdown <br/>将存在空格的单行文本分割为多行（多句）。分割后的若干行均临时采用相同时间戳，且添加了adjust_required标记提示调整时间戳避免叠轴
@@ -69,13 +70,13 @@ sub_style = "default"  # @param ["default", "ikedaCN", "kaedeCN","sugawaraCN","t
 # @markdown <br/>【注意】使用VAD filter有优点亦有缺点，请用户自行根据音频内容决定是否启用. [关于VAD filter](https://github.com/Ayanaminn/N46Whisper/blob/main/FAQ.md)
 
 
-is_vad_filter = "False" # @param ["True", "False"]
+is_vad_filter = "False"  # @param ["True", "False"]
 # @markdown  <font size="2"> *  The default <font size="3">  ```min_silence_duration``` <font size="2"> is set at 1000 ms in N46Whisper
 
-#@title **运行Whisper/Run Whisper**
-#@markdown 完成后ass文件将自动下载到本地/ass file will be auto downloaded after finish.
+# @title **运行Whisper/Run Whisper**
+# @markdown 完成后ass文件将自动下载到本地/ass file will be auto downloaded after finish.
 
-print('语音识别库配置完毕，将开始转换')
+print("语音识别库配置完毕，将开始转换")
 
 # import gc
 # assert file_name != ""
@@ -92,72 +93,73 @@ else:
         # print(file_basename)
         # print(output_dir)
     except Exception as e:
-        print(f'error: {e}')
-
+        print(f"error: {e}")
 
 
 torch.cuda.empty_cache()
-print('加载模型 Loading model...')
+print("加载模型 Loading model...")
 model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
 for i, (file_name, file_basename) in enumerate(zip(file_names, file_basenames)):
-    #Transcribe
+    # Transcribe
     if file_type == "video":
-        print('提取音频中 Extracting audio from video file...')
+        print("提取音频中 Extracting audio from video file...")
         if file_name.lower().endswith(".mp4"):
-            os.system(f'ffmpeg -i {file_name} -vn -codec copy {file_basename}.m4a')
+            os.system(f"ffmpeg -i {file_name} -vn -codec copy {file_basename}.m4a")
         else:
-            os.system(f'ffmpeg -i {file_name} -c:a aac -b:a 320k -vn {file_basename}.m4a')
-        print('提取完毕 Done.')
+            os.system(f"ffmpeg -i {file_name} -c:a aac -b:a 320k -vn {file_basename}.m4a")
+        print("提取完毕 Done.")
     # print(file_basename)
     tic = time.time()
-    print('识别中 Transcribe in progress...')
-    segments, info = model.transcribe(audio = file_name if file_type == "audio" else f"{file_basename}.m4a",
-                                        beam_size=5,
-                                        language=language,
-                                        word_timestamps=False,
-                                        vad_filter=is_vad_filter,
-                                        vad_parameters=dict(min_silence_duration_ms=1000))
+    print("识别中 Transcribe in progress...")
+    segments, info = model.transcribe(
+        audio=file_name if file_type == "audio" else f"{file_basename}.m4a",
+        beam_size=5,
+        language=language,
+        word_timestamps=False,
+        vad_filter=is_vad_filter,
+        vad_parameters=dict(min_silence_duration_ms=1000),
+    )
 
     # segments is a generator so the transcription only starts when you iterate over it
     # to use pysubs2, the argument must be a segment list-of-dicts
     total_duration = round(info.duration, 2)  # Same precision as the Whisper timestamps.
-    results= []
+    results = []
     with tqdm(total=total_duration, unit=" seconds") as pbar:
         for s in segments:
-            segment_dict = {'start':s.start,'end':s.end,'text':s.text}
+            segment_dict = {"start": s.start, "end": s.end, "text": s.text}
             results.append(segment_dict)
             segment_duration = s.end - s.start
             pbar.update(segment_duration)
 
-
-    #Time comsumed
+    # Time comsumed
     toc = time.time()
-    print('识别完毕 Done')
-    print(f'Time consumpution {toc-tic}s')
-  
+    print("识别完毕 Done")
+    print(f"Time consumpution {toc-tic}s")
+
     subs = pysubs2.load_from_whisper(results)
-    subs.save(file_basename+'.srt')
-  
+    subs.save(file_basename + ".srt")
+
     from srt2ass import srt2ass
-    ass_sub = srt2ass(file_basename + ".srt", sub_style, is_split,split_method)
-    print('ASS subtitle saved as: ' + ass_sub)
+
+    ass_sub = srt2ass(file_basename + ".srt", sub_style, is_split, split_method)
+    print("ASS subtitle saved as: " + ass_sub)
     # files.download(ass_sub)
-  
-    if export_srt == 'Yes':
+
+    if export_srt == "Yes":
         pass
         # files.download(file_basename+'.srt')
-  
-    print('第',i+1,'个文件字幕生成完毕/',i+1, 'file(s) was completed!')
+
+    print("第", i + 1, "个文件字幕生成完毕/", i + 1, "file(s) was completed!")
     torch.cuda.empty_cache()
 
-print('所有字幕生成完毕 All done!')
+print("所有字幕生成完毕 All done!")
 
 use_openai_translate = False
 if use_openai_translate:
     import openai
 
-    #@title **【实验功能】Experimental Features:**
+    # @title **【实验功能】Experimental Features:**
 
     # @markdown **AI文本翻译/AI Translation:**
     # @markdown **</br>**<font size="2"> 此功能允许用户使用AI翻译服务对识别的字幕文件做逐行翻译，并以相同的格式生成双语对照字幕。
@@ -177,10 +179,10 @@ if use_openai_translate:
     # @markdown **</br>**<font size="2"> Please input your own OpenAI API Key, then execute this cell.</font>
     # @markdown **</br>**<font size="2">【注意】 免费的API对速度有所限制，需要较长时间，用户可以自行考虑付费方案。</font>
     # @markdown **</br>**<font size="2">【Note】There are limitaions on usage for free API, consider paid plan to speed up.</font>
-    openai_key = '' # @param {type:"string"}
-    target_language = 'zh-hans'# @param ["zh-hans","english"]
-    prompt = "You are a language expert.Your task is to translate the input subtitle text, sentence by sentence, into the user specified target language.However, please utilize the context to improve the accuracy and quality of translation.Please be aware that the input text could contain typos and grammar mistakes, utilize the context to correct the translation.Please return only translated content and do not include the origin text.Please do not use any punctuation around the returned text.Please do not translate people's name and leave it as original language.\"" # @param {type:"string"}
-    temperature = 0.6 #@param {type:"slider", min:0, max:1.0, step:0.1}
+    openai_key = ""  # @param {type:"string"}
+    target_language = "zh-hans"  # @param ["zh-hans","english"]
+    prompt = "You are a language expert.Your task is to translate the input subtitle text, sentence by sentence, into the user specified target language.However, please utilize the context to improve the accuracy and quality of translation.Please be aware that the input text could contain typos and grammar mistakes, utilize the context to correct the translation.Please return only translated content and do not include the origin text.Please do not use any punctuation around the returned text.Please do not translate people's name and leave it as original language.\""  # @param {type:"string"}
+    temperature = 0.6  # @param {type:"slider", min:0, max:1.0, step:0.1}
     # @markdown <font size="4">Default prompt: </br>
     # @markdown ```You are a language expert.```</br>
     # @markdown ```Your task is to translate the input subtitle text, sentence by sentence, into the user specified target language.```</br>
@@ -193,8 +195,10 @@ if use_openai_translate:
 
     import sys
     import os
+
     # import re
     import time
+
     # import codecs
     # import regex as re
     from pathlib import Path
@@ -204,26 +208,24 @@ if use_openai_translate:
 
     # clear_output()
 
-    if sub_source == 'upload_new':
+    if sub_source == "upload_new":
         # uploaded = files.upload()
         sub_name = R""
         sub_basename = Path(sub_name).stem
-    elif sub_source == 'use_transcribed':
-        sub_name = file_basenames[0] +'.ass'
+    elif sub_source == "use_transcribed":
+        sub_name = file_basenames[0] + ".ass"
         sub_basename = file_basenames[0]
-
 
     # clear_output()
 
-    class ChatGPTAPI():
+    class ChatGPTAPI:
         def __init__(self, key, language, prompt, temperature):
             self.key = key
             # self.keys = itertools.cycle(key.split(","))
             self.language = language
             self.key_len = len(key.split(","))
-            self. prompt = prompt
+            self.prompt = prompt
             self.temperature = temperature
-
 
         # def rotate_key(self):
         #     openai.api_key = next(self.keys)
@@ -239,14 +241,11 @@ if use_openai_translate:
                         {
                             "role": "system",
                             # english prompt here to save tokens
-                            "content": f'{self.prompt}'
+                            "content": f"{self.prompt}",
                         },
-                        {
-                            "role":"user",
-                            "content": f"Original text:`{text}`. Target language: {self.language}"
-                        }
+                        {"role": "user", "content": f"Original text:`{text}`. Target language: {self.language}"},
                     ],
-                    temperature=self.temperature
+                    temperature=self.temperature,
                 )
                 t_text = (
                     completion["choices"][0]
@@ -255,7 +254,7 @@ if use_openai_translate:
                     .encode("utf8")
                     .decode()
                 )
-                total_tokens = completion['usage']['total_tokens'] # include prompt_tokens and completion_tokens
+                total_tokens = completion["usage"]["total_tokens"]  # include prompt_tokens and completion_tokens
             except Exception as e:
                 # TIME LIMIT for open api , pay to reduce the waiting time
                 sleep_time = int(60 / self.key_len)
@@ -268,14 +267,14 @@ if use_openai_translate:
                     messages=[
                         {
                             "role": "system",
-                            "content": f'{self.prompt}'
+                            "content": f"{self.prompt}"
                         },
                         {
                             "role": "user",
                             "content": f"Original text:`{text}`. Target language: {self.language}"
                         }
                     ],
-                    temperature=self.temperature
+                    temperature=self.temperature,
                 )
                 t_text = (
                     completion["choices"][0]
@@ -284,33 +283,31 @@ if use_openai_translate:
                     .encode("utf8")
                     .decode()
                 )
-            total_tokens = completion['usage']['total_tokens']
+            total_tokens = completion["usage"]["total_tokens"]
             return t_text, total_tokens
 
-
-    class SubtitleTranslator():
-        def __init__(self, sub_src, model, key, language, prompt,temperature):
+    class SubtitleTranslator:
+        def __init__(self, sub_src, model, key, language, prompt, temperature):
             self.sub_src = sub_src
-            self.translate_model = model(key, language,prompt,temperature)
+            self.translate_model = model(key, language, prompt, temperature)
             self.translations = []
             self.total_tokens = 0
 
-        def calculate_price(self,num_tokens):
-            price_per_token = 0.000002 #gpt-3.5-turbo	$0.002 / 1K tokens
+        def calculate_price(self, num_tokens):
+            price_per_token = 0.000002  # gpt-3.5-turbo	$0.002 / 1K tokens
             return num_tokens * price_per_token
 
         def translate_by_line(self):
             sub_trans = pysubs2.load(self.sub_src)
             total_lines = len(sub_trans)
-            for line in tqdm(sub_trans,total = total_lines):
+            for line in tqdm(sub_trans, total=total_lines):
                 line_trans, tokens_per_task = self.translate_model.translate(line.text)
-                line.text += (r'\N'+ line_trans)
+                line.text += r"\N" + line_trans
                 print(line_trans)
                 self.translations.append(line_trans)
                 self.total_tokens += tokens_per_task
 
             return sub_trans, self.translations, self.total_tokens
-
 
     # clear_output()
 
@@ -320,37 +317,33 @@ if use_openai_translate:
     OPENAI_API_KEY = openai_key
 
     if not OPENAI_API_KEY:
-        raise Exception(
-            "OpenAI API key not provided, please google how to obtain it"
-        )
+        raise Exception("OpenAI API key not provided, please google how to obtain it")
     # else:
     #     OPENAI_API_KEY = openai_key
 
     t = SubtitleTranslator(
         sub_src=sub_name,
-        model= translate_model,
-        key = OPENAI_API_KEY,
+        model=translate_model,
+        key=OPENAI_API_KEY,
         language=target_language,
         prompt=prompt,
-        temperature=temperature)
+        temperature=temperature
+    )
 
     translation, _, total_token = t.translate_by_line()
     total_price = t.calculate_price(total_token)
-    #Download ass file
+    # Download ass file
 
-    if output_format == 'ass':
-        translation.save(sub_basename + '_translation.ass')
+    if output_format == "ass":
+        translation.save(sub_basename + "_translation.ass")
         # files.download(sub_basename + '_translation.ass')
-    elif output_format == 'srt':
-        translation.save(sub_basename + '_translation.srt')
+    elif output_format == "srt":
+        translation.save(sub_basename + "_translation.srt")
         # files.download(sub_basename + '_translation.srt')
 
-
-
-    print('双语字幕生成完毕 All done!')
+    print("双语字幕生成完毕 All done!")
     print(f"Total number of tokens used: {total_token}")
     print(f"Total price (USD): ${total_price:.4f}")
 
     # @markdown **</br>**<font size='4'>**实验功能的开发亦是为了尝试帮助大家更有效率的制作字幕。但是只有在用户实际使用体验反馈的基础上，此应用才能不断完善，如果您有任何想法，都欢迎以任何方式联系我，提出[issue](https://github.com/Ayanaminn/N46Whisper/issues)或者分享在[讨论区](https://github.com/Ayanaminn/N46Whisper/discussions)。**
     # @markdown **</br>**<font size='4'>**The efficacy of this application cannot get improved without the feedbacks from everyday users.Please feel free to share your thoughts with me or post it [here](https://github.com/Ayanaminn/N46Whisper/discussions)**
-
--- srt2ass.py
+++ srt2ass.py
@@ -14,7 +14,7 @@ import codecs
 def fileopen(input_file):
     # use correct codec to encode the input file
     encodings = ["utf-32", "utf-16", "utf-8", "cp1252", "gb2312", "gbk", "big5"]
-    srt_src = ''
+    srt_src = ""
     for enc in encodings:
         try:
             with codecs.open(input_file, mode="r", encoding=enc) as fd:
@@ -27,8 +27,8 @@ def fileopen(input_file):
     return [srt_src, enc]
 
 
-def srt2ass(input_file, is_split = "No", split_method = "Modest", split_req = 5):
-    '''
+def srt2ass(input_file, is_split="No", split_method="Modest", split_req=5):
+    """
     Parameters:
         input_file: srt to process.
 
@@ -36,70 +36,70 @@ def srt2ass(input_file, is_split = "No", split_method = "Modest", split_req = 5)
             分割后的若干行均临时采用相同时间戳，
             且添加了adjust_required标记提示调整时间戳避免叠轴。
 
-        split_method: 
-            普通分割 (Modest): 
+        split_method:
+            普通分割 (Modest):
                 当空格后的文本长度超过 split_req 个字符，则另起一行
-            全部分割 (Aggressive): 
+            全部分割 (Aggressive):
                 只要遇到空格即另起一行
 
         split_req: 当空格后的文本长度超过 n 个字符，则另起一行
-    '''
+    """
 
-    if '.ass' in input_file:
+    if ".ass" in input_file:
         return input_file
 
     if not os.path.isfile(input_file):
-        print(input_file + ' not exist')
+        print(input_file + " not exist")
         return
 
     src = fileopen(input_file)
     srt_content = src[0]
     # encoding = src[1] # Will not encode so do not need to pass codec para
-    src = ''
-    utf8bom = ''
+    src = ""
+    utf8bom = ""
 
-    if u'\ufeff' in srt_content:
-        srt_content = srt_content.replace(u'\ufeff', '')
-        utf8bom = u'\ufeff'
-    
+    if "\ufeff" in srt_content:
+        srt_content = srt_content.replace("\ufeff", "")
+        utf8bom = "\ufeff"
+
     srt_content = srt_content.replace("\r", "")
     lines = [x.strip() for x in srt_content.split("\n") if x.strip()]
-    subLines = ''
-    dlgLines = '' # dialogue line
+    subLines = ""
+    dlgLines = ""  # dialogue line
     lineCount = 0
-    output_file = '.'.join(input_file.split('.')[:-1])
-    output_file += '.ass'
+    output_file = ".".join(input_file.split(".")[:-1])
+    output_file += ".ass"
 
     for ln in range(len(lines)):
         line = lines[ln]
-        if line.isdigit() and re.match('-?\d\d:\d\d:\d\d', lines[(ln+1)]):
+        if line.isdigit() and re.match(r"-?\d\d:\d\d:\d\d", lines[(ln + 1)]):
             if dlgLines:
                 subLines += dlgLines + "\n"
-            dlgLines = ''
+            dlgLines = ""
             lineCount = 0
             continue
         else:
-            if re.match('-?\d\d:\d\d:\d\d', line):
-                line = line.replace('-0', '0')
-                dlgLines += 'Dialogue: 0,' + line + ',Default,,0,0,0,,'
+            if re.match(r"-?\d\d:\d\d:\d\d", line):
+                line = line.replace("-0", "0")
+                dlgLines += "Dialogue: 0," + line + ",Default,,0,0,0,,"
             else:
                 if lineCount < 2:
                     dlg_string = line
-                    if is_split == "Yes" and split_method == 'Modest':
+                    if is_split == "Yes" and split_method == "Modest":
                         # do not split if space proceed and followed by non-ASC-II characters
                         # do not split if space followed by word that less than 5 characters
-                        split_string = re.sub(r'(?<=[^\x00-\x7F])\s+(?=[^\x00-\x7F])(?=\w{'+split_req+r'})', r'|', dlg_string)
+                        split_string = re.sub(r"(?<=[^\x00-\x7F])\s+(?=[^\x00-\x7F])(?=\w{" + split_req + r"})", r"|", dlg_string)
                         # print(split_string)
-                        if len(split_string.split('|')) > 1:
-                            dlgLines += (split_string.replace('|', "(adjust_required)\n" + dlgLines)) + "(adjust_required)"
+                        if len(split_string.split("|")) > 1:
+                            dlgLines += (split_string.replace("|", "(adjust_required)\n" + dlgLines)) + "(adjust_required)"
                         else:
                             dlgLines += line
-                    elif is_split == "Yes" and split_method == 'Aggressive':
+                    elif is_split == "Yes" and split_method == "Aggressive":
                         # do not split if space proceed and followed by non-ASC-II characters
                         # split at all the rest spaces
-                        split_string = re.sub(r'(?<=[^\x00-\x7F])\s+(?=[^\x00-\x7F])', r'|', dlg_string)
-                        if len(split_string.split('|')) > 1:
-                            dlgLines += (split_string.replace('|',"(adjust_required)\n" + dlgLines)) + "(adjust_required)"
+                        split_string = re.sub(r"(?<=[^\x00-\x7F])\s+(?=[^\x00-\x7F])", r"|", dlg_string)
+                        if len(split_string.split("|")) > 1:
+                            dlgLines += (split_string.replace("|", "(adjust_required)\n" + dlgLines)) + "(adjust_required)"
                         else:
                             dlgLines += line
                     else:
@@ -109,18 +109,17 @@ def srt2ass(input_file, is_split = "No", split_method = "Modest", split_req = 5)
             lineCount += 1
         ln += 1
 
-
     subLines += dlgLines + "\n"
 
-    subLines = re.sub(r'\d(\d:\d{2}:\d{2}),(\d{2})\d', '\\1.\\2', subLines)
-    subLines = re.sub(r'\s+-->\s+', ',', subLines)
+    subLines = re.sub(r"\d(\d:\d{2}:\d{2}),(\d{2})\d", "\\1.\\2", subLines)
+    subLines = re.sub(r"\s+-->\s+", ",", subLines)
     # replace style
-    subLines = re.sub(r'<([ubi])>', "{\\\\\g<1>1}", subLines)
-    subLines = re.sub(r'</([ubi])>', "{\\\\\g<1>0}", subLines)
+    subLines = re.sub(r"<([ubi])>", "{\\\\\g<1>1}", subLines)
+    subLines = re.sub(r"</([ubi])>", "{\\\\\g<1>0}", subLines)
     subLines = re.sub(r'<font\s+color="?#(\w{2})(\w{2})(\w{2})"?>', "{\\\\c&H\\3\\2\\1&}", subLines)
-    subLines = re.sub(r'</font>', "", subLines)
+    subLines = re.sub(r"</font>", "", subLines)
 
-    head_str = '''[Script Info]
+    head_str = """[Script Info]
 ; This is an Advanced Sub Station Alpha v4+ script.
 Title:
 ScriptType: v4.00+
@@ -133,15 +132,15 @@ Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour,
 Style: Default,Arial,48,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,2,2,2,10,10,10,1
 
 [Events]
-Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text'''
-    
-    output_str = utf8bom + head_str + '\n' + subLines
-    # encode again for head string
-    output_str = output_str.encode('utf8')
+Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text"""
 
-    with open(output_file, 'wb') as output:
+    output_str = utf8bom + head_str + "\n" + subLines
+    # encode again for head string
+    output_str = output_str.encode("utf8")
+
+    with open(output_file, "wb") as output:
         output.write(output_str)
 
-    output_file = output_file.replace('\\', '\\\\')
-    output_file = output_file.replace('/', '//')
+    output_file = output_file.replace("\\", "\\\\")
+    output_file = output_file.replace("/", "//")
     return output_file
